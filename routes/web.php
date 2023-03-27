377d22c062c7f19392ef1f0b5adf3b4f41ffa6b5
iqbalatma <iqbalatma@gmail.com>
2023-03-27 09:24:18 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AJAX\ProductController as AJAXProductController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ProductController::class, "index"]);
+
+Route::group([
+    "prefix" => "/ajax/products",
+    "controller" => AJAXProductController::class,
+    "as" => "ajax.products."
+], function () {
+    Route::post("/", "store")->name("store");
+});
